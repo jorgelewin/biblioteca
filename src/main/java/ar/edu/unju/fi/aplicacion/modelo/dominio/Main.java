7145530eb555a41343f25826fef7d1e1de9b0b4f
jorgelewin <jorge.lewin15@gmail.com>
2020-12-10 01:12:12 -0300

    Se ha agregado el metodo sumar

--- src/main/java/ar/edu/unju/fi/aplicacion/modelo/dominio/Main.java
+++ src/main/java/ar/edu/unju/fi/aplicacion/modelo/dominio/Main.java
@@ -3,7 +3,12 @@ package ar.edu.unju.fi.aplicacion.modelo.dominio;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(2+2);
+        System.out.println(sumar(20., 5.));
+    }
+
+    public static Double sumar(Double num1, Double num2) {
+        
+        return num1 + num2; 
     }
     
 }
